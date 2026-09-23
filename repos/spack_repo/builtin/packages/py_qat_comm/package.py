2b7243daa3c268c6e5cbbb52eb4ba9df0ec0426c
Debusschere Lyderic <lyderic.debusschere.tgcc@cea.fr>
2026-09-23 10:44:04 +0200

    ruff-check

--- repos/spack_repo/builtin/packages/py_qat_comm/package.py
+++ repos/spack_repo/builtin/packages/py_qat_comm/package.py
@@ -2,11 +2,10 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from packaging.tags import sys_tags
 from spack_repo.builtin.build_systems.python import PythonPackage
 
 from spack.package import *
-
-from packaging.tags import sys_tags
 
 
 class PyQatComm(PythonPackage):
